8c7a13d9c30484ba5041f3a8ffbb2ee10f9f746d
RodrigoSebastianPA <82163224+RodrigoSebastianPA@users.noreply.github.com>
2022-10-19 20:53:21 -0300

    Add files via upload

---
+++ busqueda.py
@@ -0,0 +1,47 @@
+from copy import deepcopy
+from Rana import Rana
+from nodo import nodo
+
+ARREGLO_FINAL = [2, 2, 2, 0, 1, 1, 1]
+
+def heuristica(arreglo):
+    punto = 0
+    for i, j in zip(arreglo, ARREGLO_FINAL):
+        if i == j:
+            punto += 1
+    return punto
+
+def mostrar_salto(mover):
+    if mover == "L" :print("salto hacia la izquierda")
+    elif mover == 'LL': print("salto doble hacia la izquierda")
+    elif mover == 'R': print("salto hacia la derecha")
+    elif mover =='RR': print("salto doble hacia la derecha")
+
+def buscar_solucion(nodo, eval_heuristica=False):
+    movimiento = ['L', 'LL', 'R', 'RR']
+
+    contador = 0
+    historial = [nodo]
+    #lista de estado y valores duplicados
+    estado = [nodo.dato]
+    while historial:
+        contador += 1
+        aux_nodo = historial.pop(0)
+
+        if aux_nodo.dato.es_solucion():
+            print("historial:", contador)
+            return aux_nodo.cadena_padres()
+        #expansion de los nodos
+        for mover in movimiento:
+            nuevo_juego = deepcopy(aux_nodo.dato)
+            nuevo_estado = nuevo_juego.mover_a(mover)
+            if nuevo_estado and not nuevo_juego in estado:
+                estado.append(nuevo_juego)
+                nodo.agregar_hijo(nuevo_juego, espacio=aux_nodo.dato)
+        #===============================================
+
+        if eval_heuristica:
+            aux_nodo.hijos.sort(key=lambda x: heuristica(x.dato.get_arreglo()), reverse=True)
+
+        historial = aux_nodo.hijos + historial
+    return []
---
+++ main.py
@@ -0,0 +1,41 @@
+from busqueda import *
+
+ARREGLO_FINAL = [2, 2, 2, 0, 1, 1, 1]
+ARREGLO_INICIAL=[1, 1, 1, 0, 2, 2, 2]
+# Creacion del juego
+juego = Rana(ARREGLO_INICIAL)
+
+print("Estado inicial:")
+print(juego)
+print('===============================================')
+
+# Empieza el estado del nodo por profundidad
+init_state = deepcopy(juego)
+juego_rana = nodo(init_state)
+print("Algoritmo por Profundidad")
+solucion = buscar_solucion(juego_rana, eval_heuristica=False)
+solucion.reverse()
+
+# Final nodo no imformada
+print("iteraciones:")
+juego_rana.mostrar_nodo()
+
+print('===============================================')
+print('Solucion:')
+for mover in solucion:
+    print(mover)
+
+init_state = deepcopy(juego)
+juego_rana = nodo(init_state)
+print("Algoritmo por Better first")
+solucion = buscar_solucion(juego_rana, eval_heuristica=True)
+solucion.reverse()
+
+# Final nodo informada
+print("iteraciones:")
+juego_rana.mostrar_nodo()
+
+print('===============================================')
+print('Solucion:')
+for mover in solucion:
+    print(mover)
